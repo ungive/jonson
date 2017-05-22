8c50958e7395819e0aa1d719dc1c9c12e6cde9df
jonas-vdberg <jonas.vanen@gmail.com>
2017-05-22 17:50:09 +0200

    Update for stream implementation and fixed copyright name.

--- stack.c
+++ stack.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2017 Jonas Berg <jonas.vanen@gmail.com>
+/* Copyright (c) 2017 Jonas van den Berg <jonas.vanen@gmail.com>
  * 
  * Jonson is free software; you can redistribute it and/or modify
  * it under the terms of the MIT license. See LICENSE for details.
@@ -6,37 +6,64 @@
 
 #include <stdlib.h>
 
-#include "jonson.h"
 #include "stack.h"
+#include "ealloc.h"
 
-void json_stack_free(struct json_stack *stack, int free_plates)
+void json_stack_free(struct json_stack *stack, int plates)
 {
-	struct json_stack *current = stack;
+	struct json_stack_node *current = stack->top;
 	while (current) {
-		if (free_plates)
+		if (plates)
 			json_free(current->data);
-		struct json_stack *next = current->next;
+		struct json_stack_node *next = current->next;
 		free(current);
 		current = next;
 	}
-}
-
-void json_stack_push(struct json_stack **stack_ptr, struct json value)
-{
-	struct json_stack *plate = emalloc(1, sizeof(struct json_stack));
-	plate->ready = 0;
-	plate->data = value;
-	plate->next = *stack_ptr;
-	*stack_ptr = plate;
-}
-
-struct json json_stack_pop(struct json_stack **stack_ptr)
-{
-	struct json_stack *stack = *stack_ptr;
-	struct json data = stack->data;
-
-	*stack_ptr = stack->next ? stack->next : NULL;
 	free(stack);
+}
 
+void json_stack_push(struct json_stack *stack, struct json value)
+{
+	struct json_stack_node *plate =
+		emalloc(1, sizeof(struct json_stack_node));
+	plate->data = value;
+	plate->next = stack->top;
+	stack->top = plate;
+}
+
+struct json json_stack_pop(struct json_stack *stack)
+{
+	if (!stack->top)
+		return JSON_NONE;
+	struct json_stack_node *top = stack->top;
+	struct json data = top->data;
+	stack->top = top->next;
+	free(top);
 	return data;
+}
+
+int json_stack_end_array(struct json_stack *stack)
+{
+	if (JSON_STACK_SEQUENCE_AV(stack->top)) {
+		struct json value = json_stack_pop(stack);
+		struct json_array *array = JSON_ARRVAL(stack->top->data);
+		json_array_add(array, value);
+		stack->top->ready = 1;
+		return 1;
+	}
+	return 0;
+}
+
+int json_stack_end_object(struct json_stack *stack)
+{
+	if (JSON_STACK_SEQUENCE_OKV(stack->top)) {
+		struct json value = json_stack_pop(stack);
+		char *key = JSON_STRVAL(json_stack_pop(stack));
+		struct json_object *object = JSON_OBJVAL(stack->top->data);
+		json_object_set(object, key, value);
+		stack->top->ready = 1;
+		free(key);
+		return 1;
+	}
+	return 0;
 }
--- stack.h
+++ stack.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2017 Jonas Berg <jonas.vanen@gmail.com>
+/* Copyright (c) 2017 Jonas van den Berg <jonas.vanen@gmail.com>
  * 
  * Jonson is free software; you can redistribute it and/or modify
  * it under the terms of the MIT license. See LICENSE for details.
@@ -7,21 +7,42 @@
 #ifndef JSON_STACK_H
 #define JSON_STACK_H
 
-struct json_stack {
+#include "jonson.h"
+
+struct json_stack_node {
 	int ready;
 	struct json data;
-	struct json_stack *next;
+	struct json_stack_node *next;
 };
 
-static inline struct json_stack *json_stack_new()
+struct json_stack {
+	struct json_stack_node *top;
+};
+
+static inline struct json_stack *json_stack_new(void)
 {
-	return NULL;
+	return ecalloc(1, sizeof(struct json_stack));
 }
 
-void json_stack_free(struct json_stack *stack, int free_plates);
+void json_stack_free(struct json_stack *stack, int plates);
 
-void json_stack_push(struct json_stack **stack_ptr, struct json value);
+void json_stack_push(struct json_stack *stack, struct json value);
+struct json json_stack_pop(struct json_stack *stack);
 
-struct json json_stack_pop(struct json_stack **stack_ptr);
+int json_stack_end_array(struct json_stack *stack);
+int json_stack_end_object(struct json_stack *stack);
 
-#endif /* STACK_H */
+/* Array->value sequence */
+#define JSON_STACK_SEQUENCE_AV(node) \
+	((node) && (node)->next && (node)->next->data.type == JSON_TYPE_ARRAY)
+
+/* Object->key sequence */
+#define JSON_STACK_SEQUENCE_OK(node) \
+	((node) && (node)->next && (node)->data.type == JSON_TYPE_STRING && \
+		(node)->next->data.type == JSON_TYPE_OBJECT)
+
+/* Object->key->value sequence */
+#define JSON_STACK_SEQUENCE_OKV(node) \
+	((node) && JSON_STACK_SEQUENCE_OK((node)->next))
+
+#endif /* JSON_STACK_H */

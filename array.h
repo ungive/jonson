fc7954b78ebf774948c08921f9b9f99c33e56d9f
jonas-git <jonas.vanen@gmail.com>
2017-05-31 22:59:39 +0200

    Removed unnecessary comments.

--- array.h
+++ array.h
@@ -19,23 +19,15 @@ struct json_array *json_array_new(void);
 
 void json_array_free(struct json_array *array);
 
-/*
- * Reserves space to fit at least 'size' elements into a [struct json_array].
- */
-void json_array_reserve(struct json_array *array, size_t size);
+int json_array_reserve(struct json_array *array, size_t size);
 
-/*
- * Resizes a [struct json_array] to 'size'.
- * Elements that excess are freed and deleted.
- */
-void json_array_resize(struct json_array *array, size_t size);
+int json_array_resize(struct json_array *array, size_t size);
 
-void json_array_add(struct json_array *array, struct json value);
+int json_array_add(struct json_array *array, struct json value);
 
-/*
- * The type of the returned value will be JSON_TYPE_NONE
- * if the index exceeds the size of the array.
- */
-struct json json_array_get(struct json_array *array, size_t index);
+static inline struct json json_array_get(struct json_array *array, size_t index)
+{
+	return index < array->size ? array->data[index] : JSON_NONE;
+}
 
 #endif /* JONSON_ARRAY_H */
